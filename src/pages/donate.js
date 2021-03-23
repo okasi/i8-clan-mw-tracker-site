cd78c1c3bc0ebaa4bd68c5c756b9133d54b88245
okasi <okans@live.se>
2021-03-23 23:24:24 +0100

    feat: remove unecessary comment from donate page

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -9,6 +9,7 @@ import {
 } from '@paypal/react-paypal-js'
 import { Spinner } from '@chakra-ui/spinner'
 import { Box, Center, Container, Heading, Text } from '@chakra-ui/layout'
+import NavBar from '../components/NavBar'
 
 export default function Donate() {
   const createSubscription = (data, actions) => {
@@ -17,28 +18,8 @@ export default function Donate() {
     })
   }
   const onApprove = (data, actions) => {
-    alert(data.subscriptionID)
+    alert("Wohoo! Subscription success! 🔥🚀🎉", data.subscriptionID)
   }
-  useEffect(() => {
-    // window.paypal
-    //   .Buttons({
-    //     style: {
-    // shape: 'pill',
-    // color: 'blue',
-    // layout: 'vertical',
-    // label: 'subscribe',
-    //     },
-    //     createSubscription: function (data, actions) {
-    //       return actions.subscription.create({
-    //         plan_id: 'P-0UD354147G029302NMA35E2I',
-    //       })
-    //     },
-    //     onApprove: function (data, actions) {
-    //       alert(data.subscriptionID)
-    //     },
-    //   })
-    //   .render('#paypal-button-container')
-  }, [])
 
   const [{ isPending }] = usePayPalScriptReducer()
 
@@ -48,51 +29,46 @@ export default function Donate() {
         <title>i8 - Donate</title>
       </Head>
 
-      <header>
-        <Container maxW="xl" centerContent color="black">
-          <Link href="/">
-            <Box m={4} _hover={{cursor: "pointer"}}>
-              <Image src="/i8_black.png" alt="i8" width={128} height={128} />
+      <NavBar />
+
+      <main>
+        <section role="hero">
+          <Container maxW="xl" centerContent color="black">
+            <Box textAlign="center">
+              <Heading variant="h1">i8 Clan</Heading>
+              <Heading variant="h2">Donate</Heading>
             </Box>
-          </Link>
-        </Container>
-      </header>
+          </Container>
+        </section>
 
-      <section role="hero">
-        <Container maxW="xl" centerContent color="black">
-          <Box textAlign="center">
-            <Heading variant="h1">i8 Clan</Heading>
-            <Heading variant="h2">Donate</Heading>
-          </Box>
-        </Container>
-      </section>
+        <section role="info">
+          <Text fontSize="xl" fontWeight="medium" textAlign="center" my={8}>
+            Donate 2.99€ a month, to make this clan the best thing since
+            Verdansk.
+          </Text>
+        </section>
 
-      <section role="info">
-        <Text fontSize="xl" fontWeight="medium" textAlign="center" my={8}>
-          Donate 2.99€ a month, to make this clan the best thing since Verdansk.
-        </Text>
-      </section>
-
-      <section role="donate">
-        <Center>
-          {isPending ? (
-            <Spinner></Spinner>
-          ) : (
-            <PayPalButtons
-              createSubscription={createSubscription}
-              onApprove={onApprove}
-              style={{
-                shape: 'pill',
-                color: 'blue',
-                layout: 'horizontal',
-                label: 'subscribe',
-                height: 55,
-                width: 256,
-              }}
-            />
-          )}
-        </Center>
-      </section>
+        <section role="donate">
+          <Center>
+            {isPending ? (
+              <Spinner></Spinner>
+            ) : (
+              <PayPalButtons
+                createSubscription={createSubscription}
+                onApprove={onApprove}
+                style={{
+                  shape: 'pill',
+                  color: 'blue',
+                  layout: 'horizontal',
+                  label: 'subscribe',
+                  height: 55,
+                  width: 256,
+                }}
+              />
+            )}
+          </Center>
+        </section>
+      </main>
     </>
   )
 }
